b0e1aece96c9893f38688b76fc148128debffd01
Adam Oleszko <adamoleszko44@gmail.com>
2019-04-15 17:53:38 +0200

    Init project

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+
+    entry: {
+        main: path.resolve('./src/js/main.js')
+    },
+
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve('./public/dist')
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+};
