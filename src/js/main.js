d6ff9d7a84e3ff422bf0a306ae465ab5730670ee
rzezniczakmateusz <rzezniczakmateusz@gmail.com>
2019-04-28 16:03:56 +0200

    get tasks

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,8 @@
 const checkButton = document.getElementsByClassName(`do`)[0];
 let checkButtonState = 'undone';
 
-<<<<<<< HEAD
-test();
-
-const checkButton = document.getElementsByClassName(`do`)[0];
-let checkButtonState = 'undone';
-
-function imageChange() {
-    if (checkButtonState === 'undone'){
-=======
 function imageChange() {
     if (checkButtonState === 'undone') {
->>>>>>> master
         checkButton.src = '../public/img/done.png';
         checkButtonState = 'done';
     } else {
@@ -23,11 +13,7 @@ function imageChange() {
 };
 
 function updateTaskStatus(id, status) {
-<<<<<<< HEAD
-    fetch(`http://localhost:3000/api/tasks/status/${id}/`, { 
-=======
     fetch(`http://localhost:3000/api/tasks/status/${id}/`, {
->>>>>>> master
         method: "PUT",
         body: {
             done: status,
@@ -39,9 +25,6 @@ function updateTaskStatus(id, status) {
         });
 }
 
-<<<<<<< HEAD
-checkButton.addEventListener('click', imageChange);
-=======
 checkButton.addEventListener('click', imageChange);
 
 /*function deleteTask(id) {
@@ -85,4 +68,106 @@ formButtonS.addEventListener('click', function (event) {
     form.style.display = 'none';
 
 });
->>>>>>> master
+
+// **********************************pobieranie taskow******************
+
+const allTasks = axios({
+    method:'get',
+    url:'http://localhost:3000/api/tasks',
+    // responseType:'json',
+  })
+    .then(function(response) {
+        for (let i = 0; i<response.data.length;i++){
+            const newParagraph = document.createElement('li');
+            newParagraph.setAttribute("id", `task${i}`);
+            newParagraph.setAttribute("class", `${response.data[i].category}`);
+            document.querySelector('.tasksList').appendChild(newParagraph);
+
+            const newTagA = document.createElement('a')
+            document.querySelector(`#task${i}`).appendChild(newTagA);
+
+            const newTagSpan = document.createElement('span')
+            document.querySelector(`#task${i}`).firstElementChild.appendChild(newTagSpan);
+            newTagSpan.innerHTML = response.data[i].describe;
+
+            const firstTagButton = document.createElement('button')
+            document.querySelector(`#task${i}`).appendChild(firstTagButton);
+
+            const imgDo = document.createElement('img');
+            imgDo.setAttribute('class', `do`);
+            imgDo.setAttribute('src', "./img/do.png")
+            document.querySelector(`#task${i}`).querySelector('button').appendChild(imgDo);
+
+            const secondTagButton = document.createElement('button')
+            document.querySelector(`#task${i}`).appendChild(secondTagButton);
+
+            const imgBin = document.createElement('img');
+            imgBin.setAttribute('class', `bin`);
+            imgBin.setAttribute('src', "./img/bin.png")
+            document.querySelector(`#task${i}`).lastElementChild.appendChild(imgBin);
+            document.querySelector(`#task${i}`).lastElementChild.querySelector('img').addEventListener('click', () => {
+                reply_click(`${response.data[i]._id}`);
+                document.querySelector(`#task${i}`).parentNode.removeChild(document.querySelector(`#task${i}`));
+            })
+        }
+  });
+
+  // ******************** pobieranie po kategerii *******************
+
+const tasksList = document.querySelector('.tasksList')
+document.querySelector(`#work_b`).addEventListener('click', () => {tasksList.innerHTML=''; setCategory('work',1)})
+document.querySelector(`#home_b`).addEventListener('click', () => {tasksList.innerHTML=''; setCategory('home',2)})
+document.querySelector(`#personal_b`).addEventListener('click', () => {tasksList.innerHTML=''; setCategory('personal',3)})
+document.querySelector(`#all_tasks_b`).addEventListener('click', () => {tasksList.innerHTML=''; setCategory('',4)})
+
+
+async function setCategory (cat,ID) {
+  
+await axios.delete(`http://localhost:3000/api/tasks/category${ID}`, { params: {category:cat} });
+// axios.get(`http://localhost:3000/api/tasks/category${ID}?category=${cat}`);
+
+axios({
+    method:'get',
+    url:`http://localhost:3000/api/tasks/category${ID}`,
+  })
+    .then(function(response) {
+        for (let i = 0; i<response.data.length;i++){
+            const newParagraph = document.createElement('li');
+            newParagraph.setAttribute("id", `task${i}`);
+            newParagraph.setAttribute("class", `${response.data[i].category}`);
+            document.querySelector('.tasksList').appendChild(newParagraph);
+
+            const newTagA = document.createElement('a')
+            document.querySelector(`#task${i}`).appendChild(newTagA);
+
+            const newTagSpan = document.createElement('span')
+            document.querySelector(`#task${i}`).firstElementChild.appendChild(newTagSpan);
+            newTagSpan.innerHTML = response.data[i].describe;
+
+            const firstTagButton = document.createElement('button')
+            document.querySelector(`#task${i}`).appendChild(firstTagButton);
+
+            const imgDo = document.createElement('img');
+            imgDo.setAttribute('class', `do`);
+            imgDo.setAttribute('src', "./img/do.png")
+            document.querySelector(`#task${i}`).querySelector('button').appendChild(imgDo);
+
+            const secondTagButton = document.createElement('button')
+            document.querySelector(`#task${i}`).appendChild(secondTagButton);
+
+            const imgBin = document.createElement('img');
+            imgBin.setAttribute('class', `bin`);
+            imgBin.setAttribute('src', "./img/bin.png")
+            document.querySelector(`#task${i}`).lastElementChild.appendChild(imgBin);
+            document.querySelector(`#task${i}`).lastElementChild.querySelector('img').addEventListener('click', () => {
+                reply_click(`${response.data[i]._id}`);
+                document.querySelector(`#task${i}`).parentNode.removeChild(document.querySelector(`#task${i}`));
+            })
+        }
+  });
+}
+
+function reply_click(clicked_id)
+{
+  axios.delete('http://localhost:3000/api/tasks/', { params: { _id: `${clicked_id}` } });
+}
